e1bb60e7b995f5b49f3fb8d8bbb8577e1679109d
jmayberry <jmayberry@wsol.com>
2017-05-22 11:39:55 -0500

    removing dependency on System.TimeZoneInfo.

--- SunriseSunset/SunriseSunsetService.cs
+++ SunriseSunset/SunriseSunsetService.cs
@@ -14,34 +14,39 @@ namespace SunriseSunset
     [Register(typeof(ISunriseSunsetService))]
     public class SunriseSunsetService : ISunriseSunsetService
     {
-        //public SunriseSunsetService(IEnumerable<ITimezoneNameTransform> transformers)
-        //{
-        //    _transformers = transformers;
-        //}
-
+        public SunriseSunsetService(IEnumerable<ITimezoneNameTransform> transformers)
+        {
+            _transformers = transformers;
+        }
+        
         public ISunriseSunsetData GetByAddress(string Address)
         {
             string latLng = GetLatLongFromAddress(Address);
+            var timezone = GetTimeZoneData(latLng);
 
             var data = new SunriseSunsetData(Address, null);
             data.LatLong = latLng;
-            data.TimeZoneName = TimeZoneData(latLng).timeZoneName;
+            data.TimeZoneName = timezone.timeZoneName;
             data.Sunrise = GetSunriseSunset(true, latLng);
             data.Sunset = GetSunriseSunset(false, latLng);
+            data.UtcOffset = timezone.rawOffset;
+            data.DstOffset = timezone.dstOffset;
 
             return data;
         }
 
         public ISunriseSunsetData GetByIP(IPAddress IpAddress)
         {
-            string latLng;
-            latLng = GetLatLongFromIP(IpAddress.ToString());
+            string latLng = GetLatLongFromIP(IpAddress.ToString());
+            var timezone = GetTimeZoneData(latLng);
 
             var data = new SunriseSunsetData(GetCityInfoFromIP(IpAddress.ToString()), IpAddress);
             data.LatLong = latLng;
-            data.TimeZoneName = TimeZoneData(latLng).timeZoneName;
+            data.TimeZoneName = timezone.timeZoneName;
             data.Sunrise = GetSunriseSunset(true, latLng);
             data.Sunset = GetSunriseSunset(false, latLng);
+            data.UtcOffset = timezone.rawOffset;
+            data.DstOffset = timezone.dstOffset;
 
             return data;
         }
@@ -63,12 +68,12 @@ namespace SunriseSunset
             {
                 DateTime? sunrise = null, sunset = null;
                 //var timeZone = GetTimeZoneInfo(LatLng);
-                var tz = TimeZoneData(LatLng);
+                var tz = GetTimeZoneData(LatLng);
 
                 foreach (var item in sunriseSunsetData.sundata)
                 {
                     if (item.phen == "R")
-                        sunrise = DateTime.Parse(item.time).AddSeconds(tz.rawOffset).AddSeconds(tz.dstOffset);
+                        sunrise = DateTime.Parse(item.time).AddSeconds(tz.rawOffset + tz.dstOffset);
 
                     if (item.phen == "S")
                         sunset = DateTime.Parse(item.time).AddSeconds(tz.rawOffset + tz.dstOffset);
@@ -184,51 +189,11 @@ namespace SunriseSunset
         }
 
         /// <summary>
-        /// Get a System.TimeZoneInfo object for given latitude and longitude corrdinates
+        /// Get a TimeZoneData object for given latitude and longitude corrdinates. With that, you can calculate the current time for the given timezone
         /// </summary>
         /// <param name="Address"></param>
         /// <returns></returns>
-        //private TimeZoneInfo GetTimeZoneInfo(string latLng)
-        //{
-        //    string cacheKey = FormatCacheKey("Timezone", latLng);
-
-        //    TimeZoneInfo timeZone = Cache.Get<TimeZoneInfo>(cacheKey);
-
-        //    if (timeZone == null)
-        //    {
-        //        // make sure the lat and long coordinates are not empty before continuing on
-        //        if (!string.IsNullOrEmpty(latLng))
-        //        {
-        //            // Google api requires the number of seconds from midnight on January 1, 1970 to get a timezone
-        //            int seconds = (int)(DateTime.Now - new DateTime(1970, 1, 1)).TotalSeconds;
-
-        //            // Google api requires a developer key for this api
-        //            string googleKey = ConfigurationManager.AppSettings.Get("GoogleKey");
-
-        //            string url = string.Format("https://maps.googleapis.com/maps/api/timezone/json?location={0}&timestamp={1}&key={2}", latLng, seconds, googleKey);
-        //            var tz = GetAsyncResult<TimeZoneData>(url);
-
-        //            if (tz != null)
-        //            {
-        //                /* May need to make some changes to the time zone name that is retrieved from Google to get the correct 
-        //                 * System.TimeZone object.
-        //                 * Needs to be Standard, not Daylight time
-        //                 * Also, Google timezone names don't always match the names in .Net
-        //                 */
-        //                var timeZoneId = TransformTimeZoneName(tz.timeZoneName);
-
-        //                timeZone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
-
-        //                if (timeZone != null)
-        //                    Cache.Set(cacheKey, timeZone, 43200);
-        //            }
-        //        }
-        //    }
-
-        //    return timeZone;
-        //}
-
-        public TimeZoneData TimeZoneData(string latLng)
+        public TimeZoneData GetTimeZoneData(string latLng)
         {
             string cacheKey = FormatCacheKey("TimeZoneData", latLng);
 
@@ -305,15 +270,15 @@ namespace SunriseSunset
         /// </summary>
         /// <param name="TimeZoneName"></param>
         /// <returns></returns>
-        //private IEnumerable<ITimezoneNameTransform> _transformers;
-        //private string TransformTimeZoneName(string TimeZoneID)
-        //{
-        //    string temp = TimeZoneID;
+        private IEnumerable<ITimezoneNameTransform> _transformers;
+        private string TransformTimeZoneName(string TimeZoneID)
+        {
+            string temp = TimeZoneID;
 
-        //    _transformers.All(x => { temp = x.Transform(temp); return true; });
+            _transformers.All(x => { temp = x.Transform(temp); return true; });
 
-        //    return temp;
-        //}
+            return temp;
+        }
 
         /// <summary>
         /// Returns a standardized cache key
